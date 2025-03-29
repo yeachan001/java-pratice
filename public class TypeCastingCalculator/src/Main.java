e454fe64a3745a3660fcdc799fb5930ba6df5bd5
yeachan001 <yeachan001@cju.ac.kr>
2025-03-29 13:45:34 +0900

    형변환
    # Conflicts:
    #	public class TypeCastingCalculator/src/Main.java

---
+++ public class TypeCastingCalculator/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        double doubleValue = 2.71828;
+
+        System.out.println("1. double -> bye: " + (byte)doubleValue);
+        System.out.println("2. doubke -> short: " +(short)doubleValue);
+        System.out.println("3. double -> int: " + (int)doubleValue);
+        System.out.println("4. double -> long: " + (long)doubleValue);
+        System.out.println("5. double -> float: " + (float)doubleValue);
+    }
+}
