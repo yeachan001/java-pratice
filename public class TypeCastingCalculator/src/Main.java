c078c7eceda49e39d960cf17609455943cded593
yeachan001 <yeachan001@cju.ac.kr>
2025-03-29 13:38:40 +0900

    형변환

--- public class TypeCastingCalculator/src/Main.java
+++ public class TypeCastingCalculator/src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double doubleValue = 2.71828;
+
+        System.out.println("1. double -> bye: " + (byte)doubleValue);
+        System.out.println("2. doubke -> short: " +(short)doubleValue);
+        System.out.println("3. double -> int: " + (int)doubleValue);
+        System.out.println("4. double -> long: " + (long)doubleValue);
+        System.out.println("5. double -> float: " + (float)doubleValue);
     }
 }
