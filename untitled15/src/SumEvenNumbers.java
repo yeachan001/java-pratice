1d30714aca42a874a832db7c80d88050218d2b68
yeachan001 <yeachan001@cju.ac.kr>
2025-05-13 11:29:40 +0900

    짝수

---
+++ untitled15/src/SumEvenNumbers.java
@@ -0,0 +1,13 @@
+public class SumEvenNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println("1부터 1000까지 짝수의 합: " + sum);
+    }
+}
